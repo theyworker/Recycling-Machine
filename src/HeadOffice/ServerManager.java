31db5dcbfb22b18d822c6c0dc5e2322bedf18f8d
theyworker <devaka.kumarasinghe@gmail.com>
2018-04-19 04:59:14 +0530

    Sinhala New Year Holiday

---
+++ src/HeadOffice/ServerManager.java
@@ -0,0 +1,21 @@
+package HeadOffice;
+
+import org.apache.xmlrpc.WebServer;
+
+public class ServerManager {
+	static int psswrdsvr = 1600;
+
+	public void runServer() {
+		//current this server is not being used 
+		WebServer server = new WebServer(1200);
+	  	 server.addHandler("hello", new HeadOffice());
+	   	 server.start();
+	}
+	
+	public void psswrdServer() 
+	{
+		WebServer server = new WebServer(psswrdsvr);
+	  	 server.addHandler("passwordmanager", new PasswordManager());
+	   	 server.start();
+	}
+}
